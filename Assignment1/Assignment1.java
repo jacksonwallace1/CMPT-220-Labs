ae83e85f9f723805948e8b1e6e9b99cea9958f67
jacksonwallace1 <jackson.wallace1@marist.edu>
2025-09-02 13:14:39 -0400

    Create Assignment1.java

---
+++ Assignment1/Assignment1.java
@@ -0,0 +1,23 @@
+//always start with importing our scanner so we can use it!
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+    //finish this one for me by receiving and printing the user's age back to them
+    Scanner sc = new Scanner(System.in);
+    System.out.println("How old are you? ");
+    String age = sc.nextLine();
+    System.out.println("You are " + age + " years old.");
+
+
+    //can you do some math for me? take these variables and print out the sum of them!
+    int num1 = 10;
+    int num2 = 37;
+    int sum = num1 + num2;
+    System.out.println(num1 + " + " + num2 + " = " + sum);
+    //?
+
+
+    // I learned the basics of Java, including how to construct classes, define variables, and read inputs
+    }
+}
